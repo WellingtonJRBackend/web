232aae764451b142f81ac157ef7be68a8bcad51b
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2022-10-20 23:14:13 -0300

    projeto criado

---
+++ src/main/java/com/projeto/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+ int i = 0;
+}
